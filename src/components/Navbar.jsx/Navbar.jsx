34012997aaaeeefcb3ad46696b455d33624177fc
Noah Lett <noahblett96@gmail.com>
2024-02-02 09:07:14 -0800

    Created initial navbar component and removed boilerplate

---
+++ src/components/Navbar.jsx/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className='nav-wrapper'>
+        <div className='nav-content'>
+          <h1>Noah Lett</h1>
+
+          <ul>
+            <li>
+              <a className='menu-item'>Home</a>
+            </li>
+            <li>
+              <a className='menu-item'>Skillset</a>
+            </li>
+            <li>
+              <a className='menu-item'>Work Experience</a>
+            </li>
+            <li>
+              <a className='menu-item'>Contact Me</a>
+            </li>
+
+            <button className='contact-btn' onClick={()=> {}}>Hire Me</button>
+          </ul>
+
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar

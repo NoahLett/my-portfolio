05937c64ba65f56f5486042dcf6706ee365da2e7
Noah Lett <noahblett96@gmail.com>
2024-02-02 11:04:52 -0800

    Desktop view navbar pretty much complete

--- src/components/Navbar.jsx/Navbar.jsx
+++ src/components/Navbar.jsx/Navbar.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import './Navbar.css';
+import { IoMenu } from "react-icons/io5";
+
 
 const Navbar = () => {
   return (
     <>
       <nav className='nav-wrapper'>
         <div className='nav-content'>
-          <h1>Noah Lett</h1>
+          <h1 className='nav-title'>Noah Lett</h1>
 
           <ul>
             <li>
               <a className='menu-item'>Home</a>
             </li>
             <li>
-              <a className='menu-item'>Skillset</a>
+              <a className='menu-item'>Stack</a>
             </li>
             <li>
               <a className='menu-item'>Work Experience</a>
@@ -25,6 +27,9 @@ const Navbar = () => {
             <button className='contact-btn' onClick={()=> {}}>Hire Me</button>
           </ul>
 
+          <div className='menu-btn'>
+            <IoMenu className='menu-icon' />
+          </div>
         </div>
       </nav>
     </>

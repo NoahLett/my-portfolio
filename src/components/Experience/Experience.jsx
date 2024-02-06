9b07706257b7ff4f43c276d99be4748ffcc4478a
Noah Lett <noahblett96@gmail.com>
2024-02-06 09:16:42 -0800

    added arrows

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -3,6 +3,7 @@ import './Experience.css';
 import { EXPERIENCE } from '../../utils/data'
 import ExperienceCard from './ExperienceCard/ExperienceCard';
 import Slider from 'react-slick';
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const Experience = () => {
 
@@ -14,7 +15,7 @@ const Experience = () => {
     speed: 500,
     slidesToShow: 2,
     slidesToScroll: 1,
-    arrows: true,
+    arrows: false,
     responsive : [
       {
         breakpoint: 769,
@@ -26,11 +27,28 @@ const Experience = () => {
     ],
   };
 
+  const slideRight = () => {
+    sliderRef.current.slickNext();
+  };
+
+  const slideLeft = () => {
+    sliderRef.current.slickPrev();
+  };
+
   return (
     <div className='experience-container'>
         <h5>Professional Experience</h5>
 
         <div className='experience-content'>
+
+          <div className='arrow-right' onClick={slideRight}>
+            <FaChevronRight />
+          </div>
+
+          <div className='arrow-left' onClick={slideLeft}>
+            <FaChevronLeft />
+          </div>
+
           <Slider ref={sliderRef} {...settings}>
             {
               EXPERIENCE.map((item) => {

ebd39bbe3a19faf725d7043ca2e0063ebc3be56d
Noah Lett <noahblett96@gmail.com>
2024-02-08 14:03:55 -0800

    Set up email messaging

--- src/components/ContactMe/ContactForm/ContactForm.jsx
+++ src/components/ContactMe/ContactForm/ContactForm.jsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './ContactForm.css';
+import emailjs from '@emailjs/browser';
 
 const ContactForm = () => {
+
+  const serviceId = import.meta.env.VITE_SERVICE_ID;
+  const templateId = import.meta.env.VITE_TEMPLATE_ID;
+  const publicKey = import.meta.env.VITE_PUBLIC_KEY;
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(serviceId, templateId, form.current, {
+        publicKey: publicKey,
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          e.target.reset();
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
     <div className='contact-form-content'>
-        <form>
+        <form ref={form} onSubmit={sendEmail}>
             <div className="name-container">
-                <input type="text" name="firstname" placeholder='First Name' />
-                <input type="text" name='lastname' placeholder='Last Name' />
+                <input type="text" name="user_name" placeholder='Your Name' />
             </div>
-            <input type="text" name='email' placeholder='Email' />
-            <textarea type="text" name="message" placeholder='Message' rows={3} />
+            <input type="email" name='user_email' placeholder='example@email.com' />
+            <textarea name="message" placeholder='Message' rows={3} />
 
             <button>SEND</button>
         </form>
